3e3e9313dba10597e951188b01e5c1940e0921c0
Fonichev.ir <fonichev.ir@edu.spbstu.ru>
2021-09-17 10:26:14 +0300

    Я учел ваше замечание и переделал задачу на 3 балла.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,9 +65,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    return hours * 60 * 60 + minutes * 60 + seconds
-}
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 60 * 60 + minutes * 60 + seconds
+
 
 
 /**
